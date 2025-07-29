6255df9bf30d868dfce0259d1af867579f934561
Junaid Abderoof <111700270+abderoofyt@users.noreply.github.com>
2025-07-30 01:08:36 +0200

    update buttons

--- WordSlasher.service.worker.js
+++ WordSlasher.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753829598|249591350';
+const CACHE_VERSION = '1753830462|1113077031';
 /** @type {string} */
 const CACHE_PREFIX = 'WordSlasher-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
