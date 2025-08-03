386d18b537f85d57b75e6998337671971bc5fdad
Junaid Abderoof <111700270+abderoofyt@users.noreply.github.com>
2025-08-03 14:46:33 +0200

    little clean up

--- WordSlasher.service.worker.js
+++ WordSlasher.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753870101|10469650095';
+const CACHE_VERSION = '1754225142|1769177148';
 /** @type {string} */
 const CACHE_PREFIX = 'WordSlasher-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
