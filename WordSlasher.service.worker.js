d52b4a400cce3c32e4dcb1fc3cb40f40439a3ed4
Junaid Abderoof <111700270+abderoofyt@users.noreply.github.com>
2025-08-17 01:53:07 +0200

    fix language change bug

--- WordSlasher.service.worker.js
+++ WordSlasher.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755238565|1487077927';
+const CACHE_VERSION = '1755388169|488018071';
 /** @type {string} */
 const CACHE_PREFIX = 'WordSlasher-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
